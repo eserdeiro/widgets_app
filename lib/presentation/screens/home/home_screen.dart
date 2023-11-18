2ca00989af02d430f94b47f755cb31b85226790d
eserdeiro <eserdeiro@gmail.com>
2023-11-18 12:23:26 -0300

    navigation drawer impl

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/widgets/home/side_menu.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -18,6 +19,7 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       body: _HomeView(),
+      drawer: const SideMenu()
     );
   }
 }
